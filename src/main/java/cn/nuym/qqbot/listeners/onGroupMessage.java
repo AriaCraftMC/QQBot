1e669ada28cbb5712b21c53fff2684d6deb3afa2
nuym <1713827227@qq.com>
2022-07-04 18:08:14 +0800

    修复绕过和Bug，添加服务器喊话

--- src/main/java/cn/nuym/qqbot/listeners/onGroupMessage.java
+++ src/main/java/cn/nuym/qqbot/listeners/onGroupMessage.java
@@ -26,38 +26,54 @@ public class onGroupMessage implements Listener {
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
         try {
-            if (e.getMessage().startsWith("#")) {
-                String command = e.getMessage().replace("#", "");
-                if (command.contains("ban") || command.contains("unmute") || command.contains("unban") || command.contains("mute") || command.contains("kick")) {
+            if (e.getMessage().startsWith("#")||e.getMessage().startsWith("＃")) {
+                String command = e.getMessage().replace("#", "").replace("＃","");
+                if (command.startsWith("ban") || command.startsWith("unmute") || command.startsWith("unban") || command.startsWith("mute") || command.startsWith("kick")) {
                     new BukkitRunnable() {
                         @Override
                         public void run() {
                             Bukkit.dispatchCommand(Bukkit.getConsoleSender(),command);
+                            success();
                         }
                     }.runTask(plugin);
+                }
+                if (command.startsWith("sc")){
+                   String command_1 = command.replace("sc","litebans broadcast &b[工作人员喊话] &r");
                     new BukkitRunnable() {
                         @Override
                         public void run() {
-                            plugin.getConfig().getLongList("bot.bot-accounts").forEach(bot -> plugin.getConfig().getLongList("bot.group-ids").forEach(group -> {
-                                try {
-                                    MiraiBot.getBot(bot).getGroup(group).sendMessageMirai("1");
-                                } catch (NoSuchElementException e) {
-                                    if (MiraiHttpAPI.Bots.containsKey(bot)) {
-                                        try {
-                                            MiraiHttpAPI.INSTANCE.sendGroupMessage(MiraiHttpAPI.Bots.get(bot), group, "1");
-                                        } catch (IOException | AbnormalStatusException ex) {
-                                            plugin.getLogger().warning("使用" + bot + "发送消息时出现异常，原因: " + ex);
-                                        }
-                                    } else plugin.getLogger().warning("指定的机器人" + bot + "不存在，是否已经登录了机器人？");
-                                }
-                            }));
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),command_1);
+                            success();
                         }
-                    }.runTaskAsynchronously(plugin);
-
+                    }.runTask(plugin);
                 }
             }
         }catch (Exception exception){
 
         }
+
+    }
+
+    private void success() {
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                plugin.getConfig().getLongList("bot.bot-accounts").forEach(bot -> plugin.getConfig().getLongList("bot.group-ids").forEach(group -> {
+                    try {
+                        MiraiBot.getBot(bot).getGroup(group).sendMessageMirai("1");
+                    } catch (NoSuchElementException e) {
+                        if (MiraiHttpAPI.Bots.containsKey(bot)) {
+                            try {
+                                MiraiHttpAPI.INSTANCE.sendGroupMessage(MiraiHttpAPI.Bots.get(bot), group, "1");
+                            } catch (IOException | AbnormalStatusException ex) {
+                                plugin.getLogger().warning("使用" + bot + "发送消息时出现异常，原因: " + ex);
+                            }
+                        } else plugin.getLogger().warning("指定的机器人" + bot + "不存在，是否已经登录了机器人？");
+                    }
+                }));
+            }
+        }.runTaskAsynchronously(plugin);
+
     }
 }
